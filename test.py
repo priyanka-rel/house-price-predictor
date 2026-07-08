49a0719554f30a9e9debb91a14899358759df983
Priyanka Naik <priyanka.naik@relanto.ai>
2026-07-08 14:50:49 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+username = "admin"
+password = "1234"
+if (username=="admin" and password=="1234"):
+    print("Login Successful")
+else:
+    if username!="admin":
+        print("Invalid Username")
